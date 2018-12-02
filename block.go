9d417d014a0875100328f507919f22fb9e57d72e
hugoroussel <hroussel96@gmail.com>
2018-12-02 23:36:46 +0100

    display method

--- block.go
+++ block.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
-	"strconv"
 	"time"
 )
 
@@ -13,23 +10,37 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
-
+	Nonce         int
 }
 
 func NewBlock(data string, prevBlockHash []byte) (newBlock *Block) {
-	newBlock = &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	newBlock.SetHash()
-	fmt.Println("Added new block with data:", data)
+	newBlock = &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(newBlock)
+	nonce, hash := pow.Run()
+	newBlock.Nonce = nonce
+	newBlock.Hash = hash
+	//newBlock.SetHash()
+	//fmt.Println("Added new block with data:", data)
 	return newBlock
 }
 
 func NewGenesisBlock() (firstBlock *Block) {
 	return NewBlock("The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", []byte{})
 }
+
+func (b *Block) Display() {
+	fmt.Println("Timestamp: ", b.Timestamp)
+	fmt.Printf("Data: %s \n", string(b.Data))
+	fmt.Printf("Prev. Block Hash: %x \n", b.PrevBlockHash)
+	fmt.Printf("Block Hash: %x \n", b.Hash)
+	fmt.Println()
+}
+
+/*
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+
+}*/
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	Blockchain := NewBlockchain()
 
 	Blockchain.AddBlock("Send 1 coin to Patrick")
 	Blockchain.AddBlock("Send 2 coins to Martin")
+
+	for _, block := range Blockchain.blocks {
+		block.Display()
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
 }
